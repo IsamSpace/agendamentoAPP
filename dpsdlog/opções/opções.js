7faa408358a5a6774c94daacce166efda7509ac7
IsamSpace <155160846+IsamSpace@users.noreply.github.com>
2024-01-19 09:30:46 -0300

    Delete dpsdlog directory

--- dpsdlog/opções/opções.js
+++
@@ -1,8 +0,0 @@
-const horaagen = [1,2,3,4,5,6,7,8,9,0]
-let a = 0
-while(a<10){
-    let ele = document.createElement("li");
-    ele.textContent = (`Horários livre hoje: ${horaagen[a]}`);
-    document.getElementById('lista').appendChild(ele);
-    a++
-}
